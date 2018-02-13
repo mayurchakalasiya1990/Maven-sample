f761deed516efd461f27d3efed2a3c71d2df2409
themayur <themayur@localhost>
2018-02-13 20:22:47 +0530

    build fail commit to check CI notification feature

--- single-module/src/main/java/com/example/Greeter.java
+++ single-module/src/main/java/com/example/Greeter.java
@@ -15,6 +15,7 @@ public class Greeter {
   //TODO: Add javadoc comment
   public String greet(String someone) {
 	  System.out.println("Hello world");
+	  int i=1/0;
     return String.format("Hello, %s!", someone);
   }
 }
